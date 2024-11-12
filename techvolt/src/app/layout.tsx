238140b650bae3efb2d795e2d627d52b605d8f6f
andre-abreu20 <andre.luismesquita@hotmail.com>
2024-11-12 11:03:58 -0300

    configurado o aviso toast

---
+++ techvolt/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Toaster } from "react-hot-toast";
+
+export const metadata: Metadata = {
+  title: "Tech Volt",
+  description: "Soluções e sistema visando a eficiência do uso energético e redução da emissão de CO2",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (  
+    <html lang="pt-br">
+      <body
+      >
+        <Toaster position="bottom-center"/>
+        {children}
+      </body>
+    </html>
+  );
+}
